633009b15ffc7b435835697e906528d73403872d
omkar4268 <omkarkore110@gmmail.com>
2025-07-17 16:06:00 +0530

    delete,constraints,serial[auto increment id's]

--- BAC.sql
+++ BAC.sql
@@ -26,3 +26,49 @@ select * from student where percentage >= 90;
  select * from student where name = 'raunak';
  select * from student where sr_no = 3;
 select name, std, percentage from student; 
+
+create table employee(sr_no int,name varchar(300) NOT NULL,city varchar(300) unique);
+insert into employee(sr_no,name,city) values (1, 'omkar','mumbai');
+insert into employee(sr_no,name,city) values (2, 'naukar','pawai');
+insert into employee(sr_no,name,city) values (3, 'rakmo','pawa');
+insert into employee(sr_no,name,city) values (3, 'rakm','paw');
+insert into employee(sr_no,city) values (3,'pawai');   --ERROR:  null value in column "name" of relation "employee" violates not-null constraint
+select * from employee order by sr_no asc;
+delete from employee where sr_no = 3;
+delete from employee where city='pawa';
+select * from employee order by city desc;
+drop table employee;
+
+
+create table employee1(sr_no int unique not null,name varchar(300) unique not null,city varchar(300) unique not null);
+insert into employee1(sr_no,name,city) values(1,'gop','wadi');
+insert into employee1(sr_no,name,city) values(2,'gopi','koliwada');
+insert into employee1(sr_no,name,city) values(3,'goi','wadsambar');
+insert into employee1(sr_no,name,city) values(4,'wani','wdi');
+delete from employee1 where sr_no = 1;
+select * from employee1;
+
+
+-- create table voter_list (
+-- 	id serial,
+-- 	name varchar(364) unique not null,
+-- 	age int check ( age >= 18),
+-- 	city varchar(1000) default 'Mumbai');
+
+create table voter_list (
+	id  serial,
+	name varchar(364) unique not null,
+	age int check ( age >= 18),
+	city varchar(1000) default 'Mumbai');
+
+
+
+insert into voter_list(name,age,city) values('ramu',18,'maheshmati');
+insert into voter_list(name,age,city) values('ramesh',19,'mahabaleshwar');
+insert into voter_list(name,age) values('ganesh',21);
+
+-- insert into voter_list(name,age) values('sk',21),('ok',21),('pk',54),('wk',54);   --never preferred
+
+select * from voter_list;
+
+drop table voter_list;
